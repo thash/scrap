056cf018b942b858837fdb6bfeab63ebb7f84867
takuyahashimoto <takuya21hashimoto@gmail.com>
2011-04-26 01:53:34 +0900

    check hoge.txt.20110425 style of date

--- scan_date.rb
+++ scan_date.rb
@@ -7,36 +7,43 @@ origin_dir = "/Users/hash/work/file-sync/origin/"
 Dir.chdir origin_dir
 origin_files = Dir.glob "./**/*"
 
+date_type = %w( AFTER_DOT BEFORE_DOT)
+date_in_name = []
+
 origin_files.each {|f|
-    # get file base-name (remove extention)
+
+    #check extension
+    ext = File.extname(f) 
+    puts "check extension ...  #{ext}"
+
+    if ext =~ /\.\d{8}/
+        puts date_type[0]
+        date_in_name << ext.scan(/\d{8}/)
+    end
+
+    #check base-name (remove extention)
     first_base = File.basename(f, ".*") 
     puts first_base
 
     # scan base-name and get date expression(s) > into Array
-    date_in_name = first_base.scan(/\d{8}/)
-    #TODO: add case name.txt.YYYYMMDD
-
-    puts "----"
-    pp date_in_name
-    puts date_in_name[0].class
-
-    if date_in_name != []
+    if first_base.scan(/\d{8}/) != []
+        puts date_type[1]
+        date_in_name << first_base.scan(/\d{8}/)
+        pp date_in_name
 
         # get last(-1st) date in filename
         date_start_index = first_base.to_s.rindex(date_in_name[-1]) 
         puts date_start_index
-
         # get from start point to last(-1)
         second_base = first_base[date_start_index .. -1] 
         puts second_base
-
         puts second_base.split("_")[1]
-        
-    else
-        next
     end
-    puts "-------------------"
-    #elsif end in YYYYMMDD
+
+    puts "date in one file \"" + f + "\" : " + date_in_name.join(",")
+
+    #elsif YYYYMMDD
     #else = there's no YYYYMMDD in file
+    puts "-------------------"
 }
 
